2c982f6b972a24145f90283ff31b67a1ef31bde3
Sam Purkis <sam.purkis@hotmail.co.uk>
2022-10-10 21:17:44 +0100

    blah

--- pieces/PawnBehaviour.py
+++ pieces/PawnBehaviour.py
@@ -10,9 +10,9 @@ class PawnBehaviour:
     ) -> Set[Position]:
         index = -1 if is_white else 1
         new_positions = [(pos[0] + index, pos[1])]
-        new_positions = {
+        new_positions = [
             p for p in new_positions if check_position_is_on_board(p, board.shape)
-        }
+        ]
         filt_all_ms = set()
         for move_pos in new_positions:
             mp_piece = board[move_pos[0]][move_pos[1]]
@@ -26,9 +26,9 @@ class PawnBehaviour:
     ) -> Set[Position]:
         index = -1 if is_white else 1
         new_positions = [(pos[0] + index, pos[1] + row_i) for row_i in (-1, 1)]
-        new_positions = {
+        new_positions = [
             p for p in new_positions if check_position_is_on_board(p, board.shape)
-        }
+        ]
         filt_all_ts = set()
         for take_pos in new_positions:
             tp_piece = board[take_pos[0]][take_pos[1]]
