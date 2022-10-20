e3ba75f0a4e477aecc16def6ab82a95c8a1f95cf
Sam Purkis <Sam.Purkis@hotmail.co.uk>
2022-10-20 13:01:08 +0000

    cythonize Array.py

--- Array.pyx
+++ Array.pyx
@@ -1,60 +1,34 @@
-from typing import Any, List, Optional, Set, Tuple, Union
 import numpy as np
-# from Array_opt import check_position_is_on_board, dist
-
-
-# class Array2D:
-#     def __init__(self, shape: Tuple[int, int] = (6, 4)) -> None:
-#         self.shape = shape
-#         self._data = []
-#         for i in range(shape[0]):
-#             self._data.append(["-" for _ in range(shape[1])])
-
-#     def __getitem__(self, index: int) -> Union[List[Any], Any]:
-#         return self._data[index]
-
-#     def __str__(self) -> str:
-#         repr_list = []
-#         for row in self._data:
-#             repr_row = []
-#             for el in row:
-#                 repr_row.append(el)
-#             repr_list.append(" ".join(map(str, repr_row)))
-#         return "\n".join(repr_list)
 
 Array2D = np.ndarray
 
 
-Position = Tuple[int, int]
-Shape = Tuple[int, int]
-
-
-def check_position_is_on_board(position: Position, board_shape: Shape) -> bool:
+cpdef bint check_position_is_on_board(tuple position, tuple board_shape):
     return (0 <= position[0] <= board_shape[0] - 1) and (
         0 <= position[1] <= board_shape[1] - 1
     )
 
 
-def dist(pos1: Position, pos2: Position) -> float:
+cpdef double dist(tuple pos1, tuple pos2):
     return abs(pos1[0] - pos2[0]) - abs(pos1[1] - pos2[1])
 
 
-def filter_positions_off_board_list(
-    positions: List[Position], board_shape: Shape
-) -> List[Position]:
+cpdef list filter_positions_off_board_list(
+    list positions, tuple board_shape
+):
     return [pos for pos in positions if check_position_is_on_board(pos, board_shape)]
 
 
-def filter_positions_off_board_set(
-    positions: Set[Position], board_shape: Shape
-) -> Set[Position]:
+cpdef set filter_positions_off_board_set(
+    set positions, tuple board_shape
+):
     return {pos for pos in positions if check_position_is_on_board(pos, board_shape)}
 
 
-def get_col_row_positions(
-    pos: Position, board_shape: Shape, max_range: Optional[int] = None
-) -> Tuple[List[List[Position]], List[List[Position]]]:
-    col_range = board_shape[0] if max_range is None else min(board_shape[0], max_range)
+cpdef tuple get_col_row_positions(
+    tuple pos, tuple board_shape, long max_range = 0
+):
+    col_range = board_shape[0] if max_range == 0 else min(board_shape[0], max_range)
     column_positions = split_at_position(
         filter_positions_off_board_list(
             [(i, pos[1]) for i in range(col_range)], board_shape
@@ -71,15 +45,15 @@ def get_col_row_positions(
     return column_positions, row_positions
 
 
-def get_diagonal_positions(
-    pos: Position, board_shape: Shape, max_range: Optional[int] = None
-) -> List[List[List[Tuple[int, int]]]]:
+cpdef list get_diagonal_positions(
+    tuple pos, tuple board_shape, int max_range = 0
+):
     diagonal_positions = []
     for a, b in ((1, 1), (1, -1), (-1, 1), (-1, -1)):
         highest_board_shape = max(*board_shape)
         diagonal_range = (
             highest_board_shape
-            if max_range is None
+            if max_range == 0
             else min(highest_board_shape, max_range)
         )
         positions = split_at_position(
@@ -93,7 +67,7 @@ def get_diagonal_positions(
     return diagonal_positions
 
 
-def get_l_positions(pos: Position, board_shape: Shape) -> Set[Position]:
+cpdef set get_l_positions(tuple pos, tuple board_shape):
     l_offsets = ((-1, 2), (1, 2), (-1, -2), (1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1))
     positions = filter_positions_off_board_set(
         {(pos[0] + i, pos[1] + j) for i, j in l_offsets}, board_shape
@@ -101,19 +75,19 @@ def get_l_positions(pos: Position, board_shape: Shape) -> Set[Position]:
     return positions
 
 
-def get_surrounding_positions(pos: Position, board_shape: Shape) -> Set[Position]:
+cpdef set get_surrounding_positions(tuple pos, tuple board_shape):
     positions = filter_positions_off_board_set(
         {(pos[0] + i, pos[1] + j) for i in (-1, 0, 1) for j in (-1, 0, 1)}, board_shape
     )
     return positions
 
 
-def split_at_position(positions: List[Position], pos: Position) -> List[List[Position]]:
+cpdef list split_at_position(list positions, tuple pos):
     index = positions.index(pos)
     return [positions[0:index], positions[index + 1 : len(positions)]]
 
 
-def sort_by_distance(pos: Position, positions: List[Position]) -> List[Position]:
+cpdef list sort_by_distance(tuple pos, list positions):
     position_distances = sorted([(dist(pos, p), p) for p in positions])
     distances = [p[1] for p in position_distances]
     return distances
